ff1fd305a45c032dc320ded36c4a277657df9195
Михаил Малафеев <mihail_malafeev@iMac-Mihail.local>
2022-08-08 15:24:34 +0400

    another try

--- base_saving.py
+++ base_saving.py
@@ -2,7 +2,7 @@
 
 
 def base_save_in_file(data_base):
-    base_file = open('data_base.txt', 'w')
+    base_file = open('data_base_info.txt', 'w')
     for i in range(len(data_base)):
         for z in range(len(data_base[i])):
             base_file.write(str(data_base[i][z]) + ", ")

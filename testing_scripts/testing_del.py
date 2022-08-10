60d5f1a901f349359b8f99793b1fa64753272fd8
Михаил Малафеев <mihail_malafeev@iMac-Mihail.local>
2022-08-10 22:16:40 +0400

    try to del person

---
+++ testing_scripts/testing_del.py
@@ -0,0 +1,10 @@
+test_arr = [[0,"Misha","Malafeev"],
+            [1,"Polina", "Mishkina"],
+            [2, "Someone", "Somes"]]
+
+del_arr = [1,"Polina", "Mishkina"]
+
+test_arr.remove(del_arr)
+
+for i in test_arr:
+    print(i)

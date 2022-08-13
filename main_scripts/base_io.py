00731acdd961e4a325b85cecf907579ec2dd3bb8
Михаил Малафеев <mihail_malafeev@iMac-Mihail.local>
2022-08-13 17:04:56 +0400

    only remains id

--- main_scripts/base_io.py
+++ main_scripts/base_io.py
@@ -1,7 +1,7 @@
 from data_form import *
 
 def print_base():
-    print(elems_of_base)
+    # print(elems_of_base)
     for z in range(len(data_base)):
         print(data_base[z])
 
--- main_scripts/base_save_del.py
+++ main_scripts/base_save_del.py
@@ -12,15 +12,21 @@ def base_save_in_file():
 
 def base_del_full():
     check_for_del = input("Type DELETE BASE (register is important) to delete all base\n")
-    if check_for_del.strip() == "DELETE BASE":
-        del_file = open(save_file_adress, 'w')
-        for elems in range(len(elems_of_base)):
-            del_file.write(elems_of_base[elems] + ", ")
-        del_file.write("\n")
-        for zeros in range(len(elems_of_base)):
-            del_file.write("0, ")
-        del_file.write("\n")
-        del_file.close()
+    while check_for_del != "DELETE BASE":
+        if check_for_del.strip() == "DELETE BASE":
+            del_file = open(save_file_adress, 'w')
+            for elems in range(len(elems_of_base)):
+                del_file.write(elems_of_base[elems] + ", ")
+            del_file.write("\n")
+            for zeros in range(len(elems_of_base)):
+                del_file.write("0, ")
+            del_file.write("\n")
+            del_file.close()
+        else:
+            print("Your answer was not like DELETE BASE")
+            break
+
+
     
 
 
